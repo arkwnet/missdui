e5500d3c5562d4b70febc8ad2301e41e34d8393c
Sora Arakawa <mail@arkw.net>
2024-12-22 23:51:46 +0900

    安装 vue-gtag

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup>
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
-import Footer from './components/Footer.vue'
-</script>
-
 <template>
   <Header />
   <Main />
   <Footer />
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { pageview } from 'vue-gtag'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+
+onMounted(() => {
+  pageview('/')
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
+import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(VueGtag, {
+    pageTrackerScreenviewEnabled: true,
+    config: { id: import.meta.env.VITE_GTAG_ID },
+  })
+  .mount('#app')
