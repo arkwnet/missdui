d067e7480cc377c5892f984ec0ee7d0eb571b4b7
Sora Arakawa <mail@arkw.net>
2024-12-22 16:35:00 +0900

    作成 Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div class="logo">
+      <img src="../assets/logo.svg" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="css">
+.header {
+  height: 80px;
+  background-color: #fff;
+}
+
+.header .logo {
+  padding-top: 10px;
+  padding-left: 20px;
+}
+
+.header .logo img {
+  height: 60px;
+}
+</style>
